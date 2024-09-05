3208cb20b925d1854780667f97681d13f5074f6d
PrysiazhnaIra <ira.prisyazhna@gmail.com>
2024-09-05 21:38:35 +0300

    contacts/readContacts

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,15 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+import path from 'node:path';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  try {
+    const filePath = path.join(process.cwd(), PATH_DB);
+    const data = await fs.readFile(filePath, 'utf-8');
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    console.error('Error reading contacts:', error);
+    throw new Error('Failed to read contacts');
+  }
+};
