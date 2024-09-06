3a8d6b1e46d9454517b51e368e0b71b61378bcf5
PrysiazhnaIra <ira.prisyazhna@gmail.com>
2024-09-06 14:31:26 +0300

    writeContacts

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,16 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+import path from 'node:path';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    const jsonData = JSON.stringify(updatedContacts, null, 2);
+    const filePath = path.join(process.cwd(), PATH_DB);
+
+    await fs.writeFile(filePath, jsonData, 'utf-8');
+    console.log('Contacts successfully written to file!');
+  } catch (error) {
+    console.error('Error writing contacts:', error);
+    throw new Error('Failed to write contacts');
+  }
+};
